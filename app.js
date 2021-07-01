9c55238a6557af624402355a9829dacac3cdbec2
FernandoFigueroa14 <luisfersurfer14@hotmail.com>
2021-06-30 23:04:43 -0500

    Añadir el proyecto de Mercado Liebre a GitHub.

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use('/static', express.static(__dirname+ '/public'));
+
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname, '/views/home.html'));
+});
+
+app.get('/home.html', function(req, res){
+    res.sendFile(path.join(__dirname, '/views/home.html'));
+});
+
+app.get('/register.html', function(req, res){
+    res.sendFile(path.join(__dirname, '/views/register.html'));
+});
+
+app.get('/login.html', function(req, res){
+    res.sendFile(path.join(__dirname, '/views/login.html'));
+});
+
+app.listen(3000, () => {
+    console.log("Servidor activo en el puerto 3000");
+});
